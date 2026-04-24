52408a5d0295bf7ceb2c0555ba2c590fda3353bb
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-24 10:39:15 +0200

    local storage mit der externen library

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import "./App.css";
 import ColorForm from "./Components/ColorForm/ColorForm";
 import { useState } from "react";
 import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
+
 function App() {
-  const [colors, setColors] = useState(initialColors);
+  const [colors, setColors] = useLocalStorageState("colors", {
+    defaultValue: initialColors,
+  });
   function handleDeleteColor(colorToDelete) {
     setColors(colors.filter((color) => color !== colorToDelete));
   }
